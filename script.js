5763104369261b409273aacf0b3739c29c9f5b3e
mancinilucas <mancini.lucasg@gmail.com>
2021-09-18 16:20:45 -0300

    maratona-discover-3.1

---
+++ script.js
@@ -0,0 +1,82 @@
+const modal = {
+  open() {
+    //abrir modal
+    //adicionar a class active ao modal
+    document.querySelector('.modal-overlay').classList.add('active')
+  },
+  close() {
+    //fechar modal
+    //remover a class active do modal
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021'
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021'
+  }
+]
+
+const Transaction = {
+  incomes() {
+    //somar as entradas
+  },
+  expenses() {
+    //somar as saídas
+  },
+  total() {
+    //remover as saídas do valor de entrada
+  }
+}
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${CSSclass}">${transaction.amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td>
+      <img src="./assets/minus.svg" alt="Remover transação" />
+    </td>
+    `
+
+    return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+  }
+}
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
